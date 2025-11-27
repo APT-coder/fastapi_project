ccb85c448b3895f5e181d962954fce988809419f
Adithya S Susa <adithya.susa@onyxcentersource.com>
2025-11-27 20:19:30 +0530

    modify: user table phone field

--- app/models/user.py
+++ app/models/user.py
@@ -8,6 +8,6 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True, nullable=False)
     full_name = Column(String, nullable=True)
-    phone = Column(String, nullable=True)
+    phone = Column(String, unique=True, index=True, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     password = Column(String, nullable=False)
