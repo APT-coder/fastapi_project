c3c47e043b5d9e5fa4d7deefbf788a530979b81a
Aswin PT <aswin.pt@onyxcentersource.com>
2025-12-08 23:59:06 +0530

    Move jwt secrets to env

--- app/core/security.py
+++ app/core/security.py
@@ -1,12 +1,16 @@
 from datetime import datetime, timedelta
 import jwt
+import os
 from passlib.context import CryptContext
-from fastapi.security import  HTTPBearer, HTTPAuthorizationCredentials
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Secret key to encode/decode JWTs
-SECRET_KEY = "your-secret-key"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 10
+SECRET_KEY = os.environ.get("SECRET_KEY", "secret-key-sample")
+ALGORITHM = os.environ.get("ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES", 10))
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 security = HTTPBearer()
