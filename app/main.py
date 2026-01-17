904acd3665a4eab34a993a996e9c6a8e5abd9ff1
Adithya S Susa <adithya.susa@experionglobal.com>
2026-01-17 13:59:25 +0530

    add: logger in main.py

--- app/main.py
+++ app/main.py
@@ -1,19 +1,26 @@
 import asyncio
+import logging
 from fastapi import FastAPI
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+)
+
 from app.background.password_expiry_worker import password_expiry_worker
 from app.routers import auth, documents, email, feedback, otp, permissions, products, roles, users
 
 app = FastAPI(title="my_fastapi_project_async")
 
+
 @app.on_event("startup")
 async def start_password_expiry_worker():
     asyncio.create_task(password_expiry_worker())
-    
+
+
 async def startup():
-    # If needed (Alembic recommended instead)
-    # async with engine.begin() as conn:
-    #     await conn.run_sync(Base.metadata.create_all)
     pass
+
 
 app.include_router(users.router, prefix="/users", tags=["Users"])
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
